33612786340587095160a7b60c98554e1246e68f
Lutfri <ardslutfi@gmail.com>
2024-01-01 20:52:34 +0700

    first upload

---
+++ App_Code/CUtility.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Linq;
+
+public class CUtility
+{
+    public CUtility()
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+
+    public static string ConvertSHa1(string str)
+    {
+        System.Text.ASCIIEncoding encoder = new System.Text.ASCIIEncoding();
+        byte[] buffer = encoder.GetBytes(str);
+        SHA1CryptoServiceProvider cryptoTransformSHA1 =
+        new SHA1CryptoServiceProvider();
+        string hash = BitConverter.ToString(
+        cryptoTransformSHA1.ComputeHash(buffer)).Replace("-", "");
+
+        return hash.ToLower();
+    }
+
+    public static DataTable Get_UserProfile(string type, string id)
+    {
+        DataTable dt = new DataTable();
+        dt.Columns.Add("ID", typeof(string));
+        dt.Columns.Add("NAME", typeof(string));
+        dt.Columns.Add("GENDER", typeof(string));
+        dt.Columns.Add("AGE", typeof(int));
+        dt.Columns.Add("ADDRESS", typeof(string));
+
+        dt.Rows.Add("01", "Lutfi", "L", 25, "Jaksel");
+        dt.Rows.Add("02", "Ardi", "L", 20, "Bekasi");
+        dt.Rows.Add("03", "Fiar", "L", 29, "Depok");
+        dt.Rows.Add("01", "Maya", "P", 25, "Bogor");
+        dt.Rows.Add("02", "Ayuning", "P", 20, "Tangerang");
+        dt.Rows.Add("03", "Tyas", "P", 29, "Serang");
+
+        var user = dt.AsEnumerable()
+            .Where(row => row.Field<string>("ID") == id && row.Field<string>("GENDER") == type)
+            .FirstOrDefault();
+
+        dt.Rows.Clear();
+        dt.ImportRow(user);
+
+        return dt;
+
+    }
+}
---
+++ Service/DataResult.aspx.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Newtonsoft.Json;
+using System.Collections;
+
+public partial class Service_DataResult : System.Web.UI.Page
+{
+    public class Param
+    {
+        public string TYPE { get; set; }
+        public string ID { get; set; }
+        public string KEY { get; set; }
+    }
+
+    public class DataResponse
+    {
+        public string CODE { get; set; }
+        public string STATUS { get; set; }
+        public string MESSAGE { get; set; }
+        public DataTable DATA { get; set; }
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        string sCallerIP;
+        if (Request.ServerVariables["HTTP_X_FORWARDED_FOR"] == null)
+        {
+            sCallerIP = Request.ServerVariables["REMOTE_ADDR"];
+        }
+        else
+        {
+            sCallerIP = Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+        }
+
+        string auth = Request.Headers["Authorization"];
+
+        Param param = new Param();
+        param.TYPE = string.IsNullOrEmpty(Request.Form["TYPE"]) ? "" : Request.Form["TYPE"];
+        param.ID = string.IsNullOrEmpty(Request.Form["ID"]) ? "" : Request.Form["ID"];
+        sendData(param, sCallerIP, "req");
+
+
+    }
+
+    private Param getParam()
+    {
+        Param param = new Param();
+        param.TYPE = string.IsNullOrEmpty(Request.Form["TYPE"]) ? "" : Request.Form["TYPE"];
+        param.ID = string.IsNullOrEmpty(Request.Form["ID"]) ? "" : Request.Form["ID"];
+
+        return param;
+    }
+
+    private void sendData(Param param, string sCallerIP, string auth)
+    {
+        Response.ContentType = "application/json";
+        DataResponse response = new DataResponse();
+        response.CODE = "00";
+        response.STATUS = "OK";
+        response.MESSAGE = "OK";
+        response.DATA = new DataTable();
+
+        string token = "";
+
+        if (auth != null)
+        {
+            try
+            {
+                token = CUtility.ConvertSHa1("REQUEST" + param.ID + auth);
+                if (token.ToUpper() != param.KEY.ToUpper())
+                {
+                    response.CODE = "01";
+                    response.STATUS = "Error";
+                    response.MESSAGE = "KEY not valid";
+                    //CUtility.ErrorLog("Hario", "KEY tidak sesuai", param.EMAIL, "AccountRegister");
+                }
+                else
+                { 
+                    DataTable dtResult = new DataTable();
+                    dtResult = CUtility.Get_UserProfile(param.TYPE, param.ID);
+                    response.DATA = dtResult;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+        else
+        {
+            response.CODE = "01";
+            response.STATUS = "Error";
+            response.MESSAGE = "Incorrect Authorization";
+        }
+
+        Response.Write(JsonConvert.SerializeObject(response));
+    }
+}
